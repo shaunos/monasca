82b1141858829ee03d017f8bc8202d81f74f5d5c
Shaun Yuan <shaun@shaunos.com>
2016-09-29 16:43:43 +0800

    init

---
+++ admin-rc.sh
@@ -0,0 +1,10 @@
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_USER_DOMAIN_NAME=default
+export OS_PROJECT_NAME=service
+export OS_USERNAME=monasca
+export OS_PASSWORD=qydcos
+export OS_AUTH_URL=http://192.168.1.11:35357/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
+
+export OS_TENANT_NAME=admin
